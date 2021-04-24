87b4c8bb12d793314a5848f357e345ba6dc75ef9
Alap Raval <alapsraval@gmail.com>
2021-04-23 23:28:20 -0500

    made changes as per discussion with Josh Furlin

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 let passwordLength = 0;
 let isLowerCase = false;
 let isUpperCase = false;
 let isNumeric = false;
 let isSpecialChar = false;
 // Array of special characters to be included in password
-var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+const specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 // join all characters in one array
 let selectedCharacters = [];
 let password = [];
@@ -31,12 +31,12 @@ function getPasswordCriteria() {
   passwordLength = prompt("Please enter desired password length between 8 and 128");
   while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength) == true) {
     passwordLength = prompt("You must enter desired password length between 8 and 128");
-    if (passwordLength === null) {
+    if (!passwordLength) {
       alert("You must enter desired password length. Please try again when you are ready.")
       break;
     }
   }
-  if (passwordLength !== null) {
+  if (passwordLength) {
     //if(passwordLength < 8 || passwordLength > 128 ) prompt("Please enter your password length");
     isLowerCase = confirm("Do you want to use lowercase?");
     isUpperCase = confirm("Do you want to use uppercase?");
@@ -58,6 +58,7 @@ function getPasswordCriteria() {
       }
     } else {
       alert("You must select at least one criteria. Please try again when you are ready.");
+      getPasswordCriteria();
     }
   }
 }
